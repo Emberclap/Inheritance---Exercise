63caba15b35bbbc0216c735f9e18cf135b7ee9f8
Emberclap <133802297+Emberclap@users.noreply.github.com>
2023-10-29 13:46:51 +0200

    Add project files.

---
+++ Person/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+       
+
+         public Person(string name, int age)
+         {
+             Name = name;
+             Age = age;
+         }
+
+         public string Name { get; set; }
+         public int Age { get; set; }
+
+         public override string ToString()
+         {
+             StringBuilder sb = new StringBuilder();
+             sb.Append($"Name: {this.Name}, Age: {this.Age}");
+
+             return sb.ToString();
+         }
+        
+    }
+}
---
+++ Restaurant/Cake.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const double CakeCalories = 1000d;
+        private const double CakeGrams = 250d;
+        private const decimal CakePrice = 5m;
+        public Cake(string name) : base(name, CakePrice, CakeGrams, CakeCalories)
+        {
+                
+        }
+        
+    }
+}
